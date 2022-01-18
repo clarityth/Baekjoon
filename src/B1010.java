4820aeb572fe83e1af5c9c510e87ddc019ceb399
TaehyungKim <netchip8@naver.com>
2022-01-18 19:42:39 +0900

    first commit

---
+++ src/B1010.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class B1010 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        int t = sc.nextInt();
+        for (int i=0; i<t; i++) {
+            int n = sc.nextInt();
+            int m = sc.nextInt();
+
+            long res = 1;
+            for (int j=m; j>n; j--){
+                res *= j;
+                res /= m-j+1;
+            }
+            sb.append(res).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
---
+++ src/B3003.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class B3003 {
+    public static void main(String[] args){
+        int [] arr = new int[]{1,1,2,2,2,8};
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        for (int i=0; i<6; i++){
+            int num = sc.nextInt();
+            arr[i] -= num;
+            sb.append(arr[i]).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
