afa503bb1439251a8328558c347c74611da90055
TaehyungKim <netchip8@naver.com>
2022-01-18 17:42:51 +0900

    first commit

---
+++ src/B2845.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class B2845 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        BigInteger n = sc.nextBigInteger();
+        BigInteger p = sc.nextBigInteger();
+        for(int i=0; i<5; i++){
+            BigInteger num = sc.nextBigInteger();
+            BigInteger res = num.subtract(n.multiply(p));
+            sb.append(res).append(" ");
+        }
+        System.out.println(sb);
+    }
+}
