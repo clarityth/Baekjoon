993981b013a2e0982b4062602f4a6e69b684223d
TaehyungKim <netchip8@naver.com>
2022-01-19 19:24:08 +0900

    first commit

---
+++ src/B1037.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class B1037 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i=0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        int max = arr[n-1];
+        int min = arr[0];
+        System.out.println(max*min);
+    }
+}
