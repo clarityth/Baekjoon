9e73ba0e84c9e5482edd21aaab9cf281fc11c1ae
TaehyungKim <netchip8@naver.com>
2022-01-20 21:39:16 +0900

    first commit

---
+++ src/B1476.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class B1476 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int E = sc.nextInt(); // 1 <= E <= 15
+        int S = sc.nextInt(); // 1 <= S <= 28
+        int M = sc.nextInt(); // 1 <= M <= 19
+        int year = 1;
+
+        int e = 1, s = 1, m = 1;
+        while (true) {
+            if (E == e && S == s && M == m) {
+                System.out.println(year);
+                break;
+            } else {
+                e += 1;
+                s += 1;
+                m += 1;
+                year += 1;
+
+                if (e > 15)
+                    e = 1;
+                if (s > 28)
+                    s = 1;
+                if (m > 19)
+                    m = 1;
+            }
+        }
+    }
+}
