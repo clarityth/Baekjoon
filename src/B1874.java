719ebfd1d52d44e88fc9f30771dd2e9da99585ae
TaehyungKim <netchip8@naver.com>
2022-01-26 21:27:26 +0900

    first commit

--- src/B1874.java
+++ src/B1874.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Stack;
 
 public class B1874 {
+    // 스택 수열
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         StringBuilder sb = new StringBuilder();
