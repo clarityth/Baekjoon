d13e4161b70e10c890fe232ae62eabb4a56edea9
TaehyungKim <netchip8@naver.com>
2022-01-29 19:23:02 +0900

    카드2

--- src/B2164.java
+++ src/B2164.java
@@ -7,13 +7,16 @@ public class B2164 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         Deque<Integer> deque = new LinkedList<>();
+
         int n = sc.nextInt();
+
         for (int i=1; i<=n; i++){
             deque.offerLast(i);
         }
-        for (int i=0; i<n-1; i++){
-            deque.pollFirst();
-            deque.offerLast(deque.pollFirst());
+
+        for (int i=0; i<n-1; i++){ // 카드가 한 장 남을 때까지 반복
+            deque.pollFirst(); // 제일 위에 있는 카드를 바닥에 버림
+            deque.offerLast(deque.pollFirst()); // 제일 위에 있는 카드를 제일 밑으로 옮김
         }
         System.out.println(deque.poll());
     }
