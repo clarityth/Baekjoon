f88e682341909d3a7732e63ce51d1ff82b563320
TaehyungKim <netchip8@naver.com>
2022-01-29 19:58:26 +0900

    막대기

--- src/B1094.java
+++ src/B1094.java
@@ -5,17 +5,13 @@ public class B1094 {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
-        int len = 64;
         int cnt = 0;
 
-        while (x > 0) {
-            if (len > x) {
-                len /= 2;
-            }
-            else {
+        while (x != 0) { // 비트마스킹
+            if (x % 2 != 0) {
                 cnt++;
-                x -= len;
             }
+            x /= 2;
         }
         System.out.println(cnt);
     }
