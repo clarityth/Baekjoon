5d4684326d72bd810fa5b50da59d6ddfda1b047f
TaehyungKim <netchip8@naver.com>
2022-02-06 21:06:26 +0900

    문자열 뒤집기

---
+++ src/B17413.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class B17413 {
+    // 문자열 뒤집기
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Stack<Character> stack = new Stack<>();
+
+        String str = br.readLine()+" ";
+        for(int i=0; i<str.length(); i++){
+            char c = str.charAt(i);
+            if (c == ' ') {
+                while (!stack.empty()){ // 단어 뒤집기
+                    sb.append(stack.pop());
+                }
+                sb.append(" ");
+            }
+            else if (c == '<'){
+                while(!stack.isEmpty()){ // 단어 뒤집기
+                    sb.append(stack.pop());
+                }
+                int idx = str.indexOf('>', i); // i 이후 처음 등장하는 >의 인덱스
+                sb.append(str, i, idx+1); // 태그는 정방향으로 삽입
+                i = idx; // 인덱스 재지정
+            }
+            else {
+                stack.push(c);
+            }
+        }
+        System.out.println(sb);
+    }
+}
