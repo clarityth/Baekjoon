3f58a1c676d179decc7feeb77c7bbcfd859ada05
TaehyungKim <netchip8@naver.com>
2022-02-09 21:33:25 +0900

    영화감독 숌

---
+++ src/B1436.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B1436 {
+    // 영화감독 숌
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        for(int i=665; cnt<n; i++){
+            if(String.valueOf(i).contains("666")){
+                cnt++;
+            }
+            if (cnt == n){
+                System.out.println(i);
+            }
+        }
+    }
+}
