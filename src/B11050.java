05725156a0f3e1b25b8d36c29f2cc208f293af6a
TaehyungKim <netchip8@naver.com>
2022-02-17 18:02:53 +0900

    이항 계수 1

--- src/B11050.java
+++ src/B11050.java
@@ -4,7 +4,7 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B11050 {
-    // 이항계수
+    // 이항 계수 1
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
