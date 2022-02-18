5f6909dc1632d1ec6be1407b2ad5754547e32974
TaehyungKim <netchip8@naver.com>
2022-02-18 22:03:01 +0900

    Hashing

--- src/B15829.java
+++ src/B15829.java
@@ -1,17 +1,22 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.*;
 
 public class B15829 {
     // Hashing
+    final static int m = 1234567891;
+    final static int r = 31;
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int l = Integer.parseInt(br.readLine());
         String str = br.readLine();
         long hash = 0;
         for(int i=0; i<l; i++){
-            hash += ((str.charAt(i)-'a'+1) * Math.pow(31,i));
+            hash += (str.charAt(i)-'a'+1)*pow(r,i);
         }
-        System.out.println(hash%1234567891);
+        System.out.println(hash%m);
+    }
+    public static long pow(int a, int b){ // Large Case : Overflow를 고려
+        return b==0 ? 1 : a * pow(a,b-1) % m;
     }
 }
