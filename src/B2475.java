b452380dd3f8a053094e0604b948f8a61a11ee80
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2022-11-01 06:23:15 +0000

    검증수

--- src/B2475.java
+++ src/B2475.java
@@ -1,14 +1,17 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringTokenizer;
 
 public class B2475 {
     // 검증수
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        int res=0;
-        for (int i=0; i<5; i++) {
-            int a = sc.nextInt();
-            res += a*a;
-        }
-        System.out.println(res%10);
+  public static void main (String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    int res = 0;
+    for (int i=0; i<5; i++) {
+      int a = Integer.parseInt(st.nextToken());
+      res += a*a;
     }
+    System.out.println(res%10);
+  }
 }
