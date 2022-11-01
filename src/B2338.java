0939695cd59b8d95e0caf164cf4583ae54032972
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2022-11-01 06:31:13 +0000

    긴자리 계산

--- src/B2338.java
+++ src/B2338.java
@@ -1,14 +1,17 @@
 import java.math.BigInteger;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class B2338 {
-    // 	긴자리 계산
+    //긴자리 계산
     public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        BigInteger a = sc.nextBigInteger();
-        BigInteger b = sc.nextBigInteger();
-        System.out.println(a.add(b));
-        System.out.println(a.subtract(b));
-        System.out.println(a.multiply(b));
+      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      
+      BigInteger a = new BigInteger(br.readLine());
+      BigInteger b = new BigINteger(br.readLine());
+      System.out.println(a.add(b));
+      System.out.println(a.subtract(b));
+      System.out.println(a.multiply(b));
     }
 }
