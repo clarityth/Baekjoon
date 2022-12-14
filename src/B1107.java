719fa976316c8a7166003910a7829fc4a1616165
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2022-12-14 04:02:08 +0000

    리모컨

--- src/B1107.java
+++ src/B1107.java
@@ -4,13 +4,14 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 import java.lang.Math;
 
-public class B1107 {
+public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     boolean[] brokenBtn = new boolean[10];
     int N = Integer.parseInt(br.readLine());
     int M = Integer.parseInt(br.readLine());
     if (M > 0) {
+      // 고장난 버튼의 개수가 0보다 클때만 고장난 버튼을 입력받음
       StringTokenizer st = new StringTokenizer(br.readLine());
       for (int i = 0; i < M; ++i) {
         brokenBtn[Integer.parseInt(st.nextToken())] = true;
@@ -20,7 +21,9 @@ public class B1107 {
       System.out.println("0");
       System.exit(0);
     }
-    int res = Math.abs(N - 100), pressCnt = 0, channelLen = 0;
+    // res의 초깃값 = 처음 채널인 100에서부터 +, - 버튼을 눌러야하는 횟수
+    int res = Math.abs(N - 100);
+    int pressCnt = 0, channelLen = 0;
 
     for (int i = 0; i < 1000001; ++i) {
       int c = i;
@@ -29,20 +32,29 @@ public class B1107 {
       // 0일때 예외처리
       if (c == 0) {
         if (brokenBtn[0]) {
-          channelLen = 0;
+          // 0 버튼이 고장난 경우
+          isCanPress = false;
         } else {
+          // 0 버튼이 고장나지 않은 경우
           channelLen = 1;
         }
+      } else {
+        // 각 자릿수의 버튼들을 전부 누를 수 있는지 체크
+        while (c > 0) {
+          if (brokenBtn[c % 10]) {
+            // 버튼이 고장난 경우
+            isCanPress = false;
+            break;
+          }
+          c /= 10;
+          ++channelLen;
+        }
       }
-      while (c > 0) {
-        if (brokenBtn[c % 10])
-          isCanPress = false;
-        c /= 10;
-        ++channelLen;
-      }
-      if (isCanPress) {
+      if (isCanPress && channelLen > 0) {
+        // pressCnt = 인접한 수에서 시작해서 +, - 버튼을 눌러야 하는 횟수
         pressCnt = Math.abs(i - N);
         if (res > pressCnt + channelLen) {
+          // 처음 채널(100)과 인접한 수에서 시작한 횟수 비교
           res = pressCnt + channelLen;
         }
       }
