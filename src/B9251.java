2baf5816de8ac9ca916a4652a1012137e706d58d
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2022-12-27 00:31:34 +0000

    LCS

--- src/B9251.java
+++ src/B9251.java
@@ -11,16 +11,16 @@ public class B9251 {
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     String str1 = br.readLine();
     String str2 = br.readLine();
-    int[][] table = new int[str1.length()+1][str2.length()+1];
+    int[][] lcs = new int[str1.length()+1][str2.length()+1];
     for(int i=1; i<=str1.length(); ++i){
       for(int j=1; j<=str2.length(); ++j){
         if (str1.charAt(i-1) == str2.charAt(j-1))
-          table[i][j] = table[i-1][j-1] + 1;
+          lcs[i][j] = lcs[i-1][j-1] + 1;
         else 
-          table[i][j] = Math.max(table[i-1][j], table[i][j-1]);
+          lcs[i][j] = Math.max(lcs[i-1][j], lcs[i][j-1]);
       }
     }
-    bw.write(String.valueOf(table[str1.length()][str2.length()]));
+    bw.write(String.valueOf(lcs[str1.length()][str2.length()]));
     bw.flush();
     br.close();
     bw.close();
