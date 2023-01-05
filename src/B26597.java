e825a10830ec7c47557c43987c166b83726305a2
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2023-01-05 08:22:38 +0000

    이 사람 왜 이렇게 1122를 좋아함?

--- src/B26597.java
+++ src/B26597.java
@@ -12,32 +12,36 @@ public class B26597 {
     int Q = Integer.parseInt(br.readLine());
     long min = -1 * (long) Math.pow(10, 18) - 1;
     long max = (long) Math.pow(10, 18) + 1;
-    boolean isAppended = false, isFinalized = false;
+    boolean isAppended = false, isParadox = false;
     int criticalQNum = 1;
     for (int i = 1; i <= Q; ++i) {
       StringTokenizer st = new StringTokenizer(br.readLine());
       long x = Long.parseLong(st.nextToken());
       String special = st.nextToken();
+      
+      // 최댓값, 최솟값들의 범위를 줄여감
       if (special.equals("^")) 
         min = Math.max(x, min);
-      
       else if (special.equals("v")) 
         max = Math.min(x, max);
       
       long diff = max - min;
+      /* 질문에 나온 수는 정답이 아니므로, 차이가 2인 정수가 정답 
+       -> 정답을 구한 이후에도 모순이 발생할 수 있으므로 break 하지 않음 */
       if (diff == 2 && !isAppended) {
         criticalQNum = i;
         isAppended = true;
       }
       else if ((diff < 2)){
+      // 모순이 발생하지 않는다면, diff는 항상 2 이상이어야 함.
         bw.write("Paradox!\n" + String.valueOf(i));
-        isFinalized = true;
+        isParadox = true;
         break;
       }
     }
-    if (!isAppended && !isFinalized)
+    if (!isAppended && !isParadox)
       bw.write("Hmm...");
-    else if (isAppended && !isFinalized)
+    else if (isAppended && !isParadox)
       bw.write("I got it!\n" + String.valueOf(criticalQNum));
     bw.flush();
     bw.close();
