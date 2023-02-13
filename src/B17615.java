0e1c4e540bfd86e5bdb2d9f5793c35cd45f13163
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2023-02-13 11:26:05 +0000

    볼 모으기

--- src/B17615.java
+++ src/B17615.java
@@ -12,19 +12,17 @@ public class B17615 {
     int redLeftCnt = 0, redRightCnt = 0, blueLeftCnt = 0, blueRightCnt = 0;
     for (int i = 0; i < N; ++i) {
       char color = str.charAt(i);
-      // 파란공을 왼쪽으로 이동시키는 경우
-      if (color == 'B'){
-        isBlueAppeared = true;
-        if (isRedAppeared) {
-          isRedAppeared = false;
-          blueLeftCnt++;
-        }
       // 빨간공을 왼쪽으로 이동시키는 경우
-      } else {
+      if (color == 'R'){
         isRedAppeared = true;
-        if (isBlueAppeared){
-          isBlueAppeared = false;
+        if (isBlueAppeared) {
           redLeftCnt++;
+        }
+      // 파란공을 왼쪽으로 이동시키는 경우
+      } else {
+        isBlueAppeared = true;
+        if (isRedAppeared){
+          blueLeftCnt++;
         }
       }
     }
@@ -32,28 +30,22 @@ public class B17615 {
     isRedAppeared = false;
     for (int i = N-1; i >= 0; --i) {
       char color = str.charAt(i);
-      // 파란공을 오른쪽으로 이동시키는 경우
-      if (color == 'B'){
-        isBlueAppeared = true;
-        if (isRedAppeared) {
-          isRedAppeared = false;
-          blueRightCnt++;
+      // 빨간공을 오른쪽으로 이동시키는 경우
+      if (color == 'R'){
+        isRedAppeared = true;
+        if (isBlueAppeared) {
+          redRightCnt++;
         }
       // 파란공을 오른쪽으로 이동시키는 경우
       } else {
-        isRedAppeared = true;
-        if (isBlueAppeared){
-          isBlueAppeared = false;
+        isBlueAppeared = true;
+        if (isRedAppeared){
           blueRightCnt++;
         }
       }
     }
-    System.out.println(blueLeftCnt);
-    System.out.println(blueRightCnt);
-    System.out.println(redLeftCnt);
-    System.out.println(redRightCnt);
-    int min = Math.min(blueRightCnt, Math.min(blueLeftCnt, Math.min(redRightCnt, redLeftCnt)));
-    bw.write(String.valueOf(min));
+    int minCnt = Math.min(blueRightCnt, Math.min(blueLeftCnt, Math.min(redRightCnt, redLeftCnt)));
+    bw.write(String.valueOf(minCnt));
     bw.flush();
     bw.close();
     br.close();
