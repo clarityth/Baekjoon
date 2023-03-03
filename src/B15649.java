fe1ab3d4a942fd42c762fdf1b059fd18a42704e0
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2023-03-03 06:42:53 +0000

    N과 M (1)

---
+++ src/B15649.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+public class B15649 {
+  static int N, M;
+  static StringBuilder sb;
+  static void permutation(int[] num, int[] output, boolean visited[], int depth, int idx, int r){
+    if (depth == r){
+      for (int i = 0; i < r; ++i){
+        sb.append(output[i] + " ");
+      }
+      sb.append("\n");
+      return;
+    }
+    
+    for (int i = 0; i < N; ++i) {
+      if (!visited[i]){
+        visited[i] = true;
+        output[depth] = num[i];
+        permutation(num, output, visited, depth + 1, i, r);
+        visited[i] = false;
+      }
+    }
+  }
+  public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    sb = new StringBuilder();
+    int num[] = new int[N];
+    int output[] = new int[N];
+    boolean visited[] = new boolean[N];
+    for (int i = 0; i < N; ++i){
+      num[i] = i + 1;
+    }
+    permutation(num, output, visited, 0, 0, M);
+    bw.write(sb.toString());
+    bw.flush();
+    bw.close();
+    br.close();
+  }
+}
