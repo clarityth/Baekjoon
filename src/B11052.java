2147b4ef9135ecf244aa0c4a923cc8d0601ecfe6
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2023-03-20 04:24:14 +0000

    카드 구매하기

--- src/B11052.java
+++ src/B11052.java
@@ -15,7 +15,10 @@ public class B11052 {
       prices[i] = Integer.parseInt(st.nextToken());
     }
     for (int i = 1; i <= N; ++i) {
+      // i = 사야하는 카드의 갯수
       for (int j = 1; j <= i; ++j){
+        // j = 사려고 하는 카드팩의 번호
+        // 현재 i개의 카드를 사기 위해 1~i번 카드팩을 사고, 남은 금액을 채워 넣을 때(이전에 구했던 DP 값)의 기댓값과 기존 값을 비교
         DP[i] = Math.max(prices[j] + DP[i-j], DP[i]);
       }
     }
