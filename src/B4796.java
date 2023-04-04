7a76defe4baa51bb043f39ee4126e251c7418a31
TaehyungKim <91403729+clarityth@users.noreply.github.com>
2023-04-04 05:08:18 +0000

    캠핑

--- src/B4796.java
+++ src/B4796.java
@@ -1,9 +1,9 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class B4796 {
 
-  public static void B4796(String[] args) throws IOException {
+  public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     String str = br.readLine();
