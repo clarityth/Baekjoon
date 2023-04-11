9071e0f7af052f05c2c30998c0692eebadfe7ace
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-11 05:11:12 +0000

    녹색 옷 입은 애가 젤다지?

--- src/B4485.java
+++ src/B4485.java
@@ -6,32 +6,32 @@ public class B4485 {
   static class pos{
     int x;
     int y;
-    int rupy;
-    public pos (int x, int y, int rupy) {
+    int rupee;
+    public pos (int x, int y, int rupee) {
       this.x = x;
       this.y = y;
-      this.rupy = rupy;
+      this.rupee = rupee;
     }
   }
-  static void bfs(int startX, int startY) {
-    Queue<pos> q = new LinkedList<pos>();
-    blackRupy[startY][startX] = cave[startY][startX];
+  static void djikstra(int startX, int startY) {
+    PriorityQueue<pos> q = new PriorityQueue<pos>((o1, o2) -> o1.rupee - o2.rupee);
+    blackRupee[startY][startX] = cave[startY][startX];
     q.offer(new pos(startX, startY, cave[startY][startX]));
     while (!q.isEmpty()) {
       pos now = q.poll();
       int nowX = now.x;
       int nowY = now.y;
-      int nowRupy = now.rupy;
-      if (nowRupy > blackRupy[nowY][nowX])
+      int nowRupee = now.rupee;
+      if (nowRupee > blackRupee[nowY][nowX])
         continue;
       for (int i = 0; i < 4; ++i) {
         int nextX = nowX + dx[i];
         int nextY = nowY + dy[i];
         if (nextX >= N || nextY >= N || nextX < 0 || nextY < 0)
           continue;
-        if (blackRupy[nextY][nextX] > nowRupy + cave[nextY][nextX]) {
-          blackRupy[nextY][nextX] = nowRupy + cave[nextY][nextX];
-          q.offer(new pos(nextX, nextY, nowRupy + cave[nextY][nextX]));
+        if (blackRupee[nextY][nextX] > nowRupee + cave[nextY][nextX]) {
+          blackRupee[nextY][nextX] = nowRupee + cave[nextY][nextX];
+          q.offer(new pos(nextX, nextY, nowRupee + cave[nextY][nextX]));
         }
       }
     }
@@ -41,7 +41,7 @@ public class B4485 {
   static final int dy[] = {1, -1, 0, 0};
   static int N;
   static int cave[][];
-  static int blackRupy[][];
+  static int blackRupee[][];
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -53,16 +53,16 @@ public class B4485 {
         break;
       ++idx;
       cave = new int[N][N];
-      blackRupy = new int[N][N];
+      blackRupee = new int[N][N];
       for (int i = 0; i < N; ++i) {
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int j = 0; j < N; ++j) {
           cave[i][j] = Integer.parseInt(st.nextToken());
-          blackRupy[i][j] = INF;
+          blackRupee[i][j] = INF;
         }
       }
-      bfs(0, 0);
-      sb.append("Problem " + idx + ": " + blackRupy[N-1][N-1] + "\n");
+      djikstra(0, 0);
+      sb.append("Problem " + idx + ": " + blackRupee[N-1][N-1] + "\n");
     }
     bw.flush();
     bw.write(sb.toString());
