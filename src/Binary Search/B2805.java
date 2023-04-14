8614090ee27a6db121d166e1e3073c2766b17af8
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-14 12:14:24 +0000

    나무 자르기

--- src/Binary Search/B2805.java
+++ src/Binary Search/B2805.java
@@ -1,10 +1,10 @@
+// 나무 자르기
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B2805 {
-    // 나무 자르기
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
