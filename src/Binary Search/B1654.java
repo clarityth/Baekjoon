c3fa9959c55267536dfd1766ec8f8f35d6fb81e5
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-14 12:13:03 +0000

    랜선 자르기

--- src/Binary Search/B1654.java
+++ src/Binary Search/B1654.java
@@ -6,7 +6,6 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class B1654 {
-    // 랜선자르기
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
