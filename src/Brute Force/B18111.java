4e2d19f4c6ba62c2bc7f604eda2f32890b2fa929
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-15 09:57:57 +0000

    마인크래프트

--- src/Brute Force/B18111.java
+++ src/Brute Force/B18111.java
@@ -1,10 +1,10 @@
+// 마인크래프트
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B18111 {
-    // 마인크래프트
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
