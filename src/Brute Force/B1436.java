2dad26e3efa07a733cb30b60d402ae6e7a65a80e
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-15 09:48:23 +0000

    // 영화감독 숌

--- src/Brute Force/B1436.java
+++ src/Brute Force/B1436.java
@@ -1,9 +1,9 @@
+// 영화감독 숌
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class B1436 {
-    // 영화감독 숌
     public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
