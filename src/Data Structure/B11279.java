477e64e8c3641063a0b5fea53f08e2597d04fc92
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-28 10:24:54 +0000

    최대 힙

--- src/Data Structure/B11279.java
+++ src/Data Structure/B11279.java
@@ -1,27 +1,26 @@
+// 최대 힙
 import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class B11279 {
-    // 최대 힙
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        PriorityQueue <Integer> q = new PriorityQueue<>();
+  public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    PriorityQueue <Integer> q = new PriorityQueue<>();
 
-        for (int i=0; i<n; i++){
-            int x = sc.nextInt();
-            if (x == 0) {
-                if (q.size() == 0) {
-                    System.out.println("0");
-                }
-                else {
-                    System.out.println(-q.poll()); // -를 붙여서 출력 (본래 값으로 출력)
-                }
-            }
-            else if (x > 0) {
-                q.offer(-x); // -를 붙여서 힙에 우선순위 큐에 저장 (가장 큰 값이 가장 작은 값이 됨)
-            }
+    for (int i = 0; i < n; ++i){
+      int x = sc.nextInt();
+      if (x == 0) {
+        if (q.size() == 0) {
+          System.out.println("0");
         }
-
+        else {
+          System.out.println(-q.poll()); // -를 붙여서 출력 (본래 값으로 출력)
+        }
+      }
+      else if (x > 0) {
+        q.offer(-x); // -를 붙여서 힙에 우선순위 큐에 저장 (가장 큰 값이 가장 작은 값이 됨)
+      }
     }
+  }
 }
