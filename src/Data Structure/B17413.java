796b07828c84ee8db8613b79971d07e0eb810dba
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-28 10:40:46 +0000

    단어 뒤집기 2

--- src/Data Structure/B17413.java
+++ src/Data Structure/B17413.java
@@ -1,36 +1,38 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
+// 단어 뒤집기 2 
+import java.io.*;
+import java.util.*;
 
 public class B17413 {
-    // 문자열 뒤집기
-    public static void main(String[] args)throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        Stack<Character> stack = new Stack<>();
+  public static void main(String[] args)throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    StringBuilder sb = new StringBuilder();
+    Stack<Character> stack = new Stack<>();
 
-        String str = br.readLine()+" ";
-        for(int i=0; i<str.length(); i++){
-            char c = str.charAt(i);
-            if (c == ' ') {
-                while (!stack.empty()){ // 단어 뒤집기
-                    sb.append(stack.pop());
-                }
-                sb.append(" ");
-            }
-            else if (c == '<'){
-                while(!stack.isEmpty()){ // 단어 뒤집기
-                    sb.append(stack.pop());
-                }
-                int idx = str.indexOf('>', i); // i 이후 처음 등장하는 >의 인덱스
-                sb.append(str, i, idx+1); // 태그는 정방향으로 삽입
-                i = idx; // 인덱스 재지정
-            }
-            else {
-                stack.push(c);
-            }
+    String str = br.readLine() + " ";
+    for (int i = 0; i < str.length(); ++i) {
+      char c = str.charAt(i);
+      if (c == ' ') {
+        while (!stack.empty()) { // 단어 뒤집기
+          sb.append(stack.pop());
         }
-        System.out.println(sb);
+        sb.append(" ");
+      }
+      else if (c == '<'){
+        while (!stack.isEmpty()) { // 단어 뒤집기
+          sb.append(stack.pop());
+        }
+        int idx = str.indexOf('>', i); // i 이후 처음 등장하는 >의 인덱스
+        sb.append(str, i, idx + 1); // 태그는 정방향으로 삽입
+        i = idx; // 인덱스 재지정
+      }
+      else {
+        stack.push(c);
+      }
     }
+    bw.flush();
+    bw.write(sb.toString());
+    bw.close();
+    br.close();
+  }
 }
