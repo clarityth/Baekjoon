d3290498dce27d022888461444805e82e125f48e
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-28 10:42:48 +0000

    비밀번호 찾기

--- src/Data Structure/B17219.java
+++ src/Data Structure/B17219.java
@@ -2,7 +2,7 @@
 import java.util.*;
 import java.io.*;
 
-public class Main {
+public class B17219 {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
