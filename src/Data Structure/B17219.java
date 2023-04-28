6fbb84d8b27536abc6eb725aa0c78f373be795bb
clarityth <91403729+clarityth@users.noreply.github.com>
2023-04-28 10:34:02 +0000

    비밀번호 찾기

--- src/Data Structure/B17219.java
+++ src/Data Structure/B17219.java
@@ -1,27 +1,31 @@
-import java.util.HashMap;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-import java.io.BufferedReader;
-import java.io.IOException;
+// 비밀번호 찾기
+import java.util.*;
+import java.io.*;
 
-public class B17219{
+public class Main {
   public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
     StringTokenizer st = new StringTokenizer(br.readLine());
     StringBuilder sb = new StringBuilder();
     int N = Integer.parseInt(st.nextToken());
     int M = Integer.parseInt(st.nextToken());
+    
     HashMap<String, String> hm = new HashMap<>();
-    for(int i=0; i<N; i++){
+    for (int i = 0; i < N; ++i){
       st = new StringTokenizer(br.readLine());
       String site = st.nextToken();
       String pw = st.nextToken();
       hm.put(site, pw);
     }
-    for(int i=0; i<M; i++){
+    
+    for (int i = 0; i < M; ++i){
       String searchSite = br.readLine();
-      sb.append(hm.get(searchSite)+"\n");
+      sb.append(hm.get(searchSite) + "\n");
     }
-    System.out.println(sb);
+    bw.flush();
+    bw.write(sb.toString());
+    bw.close();
+    br.close();
   }
 }
