62a95f54b7566f3abf81b560ae2250f77bfcb1e6
clarityth <91403729+clarityth@users.noreply.github.com>
2023-05-01 10:07:02 +0000

    가희와 키워드

--- src/Data Structure/B22233.java
+++ src/Data Structure/B22233.java
@@ -1,3 +1,4 @@
+// 가희와 키워드
 import java.io.*;
 import java.util.*;
 
@@ -10,11 +11,11 @@ public class B22233 {
     int M = Integer.parseInt(st.nextToken());
     HashMap<String, Boolean> keyWordMap = new HashMap<String, Boolean>();
     // hashMap에 <단어, true>를 저장 -> getOrDefault() 탐색 실패시 false를 기본값으로 설정
-    for (int i=0; i<N; ++i){
+    for (int i = 0; i < N; ++i){
       String word = br.readLine();
       keyWordMap.put(word, true);
     }
-    for (int i=0; i<M; ++i){
+    for (int i = 0; i < M; ++i){
       String usedWordStr = br.readLine();
       String[] usedWordArr = usedWordStr.split(",");
       // , 를 기준으로 문자열 파싱
