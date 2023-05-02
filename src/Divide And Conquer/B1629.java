5672357bca04bbb5dd1e15518d7fa85f2d103459
clarityth <91403729+clarityth@users.noreply.github.com>
2023-05-02 12:59:48 +0000

    곱셈

--- src/Divide And Conquer/B1629.java
+++ src/Divide And Conquer/B1629.java
@@ -1,3 +1,4 @@
+// 곱셈
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.InputStreamReader;
@@ -11,10 +12,14 @@ public class B1629 {
       return n % mod;
 
     else {
-      long x = power(n, exp/2, mod);
+      // 지수를 절반으로 줄여가며 재귀 호출
+      long x = power(n, exp / 2, mod);
+      // 지수가 홀수일 경우 n을 한번 더 곱해주고, 모듈러 연산 수행
       if (exp % 2 == 1){
+        // 모듈러 성질: (a*b)%c = (a%c * b%c)%c
         return (x * x % mod) * n % mod;
       }
+      // 지수가 짝수일 경우
       else {
         return x * x % mod;
       }
