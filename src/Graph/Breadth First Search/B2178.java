e8b5866500afd65bd9621e7917873db97fd399e5
clarityth <91403729+clarityth@users.noreply.github.com>
2023-05-05 06:41:34 +0000

    미로 탐색

--- src/Graph/Breadth First Search/B2178.java
+++ src/Graph/Breadth First Search/B2178.java
@@ -1,3 +1,4 @@
+// 미로 탐색
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -5,13 +6,13 @@ import java.util.Queue;
 import java.util.LinkedList;
 import java.io.IOException;
   
-public class B2178{
+public class B2178 {
   public static int N, M;
   public static int maze[][];
   public static int route[][];
   public static boolean visit[][];
-  public static int dx[] = {0,0,-1,1};
-  public static int dy[] = {1,-1,0,0};
+  public static int dx[] = {0, 0, -1, 1};
+  public static int dy[] = {1, -1, 0, 0};
 
   public static void bfs(int startX, int startY){
     Queue<int[]> q = new LinkedList<int[]>();
@@ -22,11 +23,9 @@ public class B2178{
       int current[] = q.poll();
       int currentX = current[0];
       int currentY = current[1];
-      
-      for(int i=0; i<4; i++){
+      for (int i = 0; i < 4; ++i){
         int nextX = currentX + dx[i];
         int nextY = currentY + dy[i];
-        
         if (nextX >= M || nextY >= N || nextX < 0 || nextY < 0 || maze[nextY][nextX] == 0 || visit[nextY][nextX])
           continue;
         else {
@@ -45,13 +44,13 @@ public class B2178{
     maze = new int[101][101];
     visit = new boolean[101][101];
     route = new int[101][101];
-    for(int i=0; i<N; i++){
+    for(int i = 0; i < N; i++){
       String input = br.readLine();
-      for(int j=0; j<M; j++){
-        maze[i][j] = input.charAt(j)-'0';
+      for(int j = 0; j < M; j++){
+        maze[i][j] = input.charAt(j) - '0';
       }
     }
-    bfs(0,0);
-    System.out.print(route[N-1][M-1]+1);
+    bfs(0, 0);
+    System.out.print(route[N - 1][M - 1] + 1);
   }
 }
