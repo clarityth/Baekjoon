1a00a6ac4c393a96775fa363d89587a9eb71994a
clarityth <91403729+clarityth@users.noreply.github.com>
2023-12-13 01:14:22 +0000

    연결 요소의 개수

--- src/Graph/Depth First Search/B11724.java
+++ src/Graph/Depth First Search/B11724.java
@@ -1,31 +1,30 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.StringTokenizer;
+// 연결 요소의 개수
+import java.io.*;
+import java.util.*;
 
 public class B11724 {
   public static int N, M;
   public static int[][] graph;
   public static boolean[] visited;
 
-  public static void dfs(int start){
+  public static void dfs(int start) {
     visited[start] = true;
-    for(int i=1; i<=N; i++){
-      if(graph[start][i] == 1 && !visited[i]){
+    for(int i = 1; i <= N; ++i) {
+      if(graph[start][i] == 1 && !visited[i]) {
         dfs(i);
       }
     }
   }
-  public static void main (String[] args) throws IOException{
+  public static void main (String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     StringTokenizer st = new StringTokenizer(br.readLine());
     N = Integer.parseInt(st.nextToken());
     M = Integer.parseInt(st.nextToken());
-    int conectedComponent = 0;
-    graph = new int[N+1][N+1];
-    visited = new boolean[N+1];
+    int connectedComponent = 0;
+    graph = new int[N + 1][N + 1];
+    visited = new boolean[N + 1];
     
-    for(int i=0; i<M; i++){
+    for (int i = 0; i < M; ++i) {
       st = new StringTokenizer(br.readLine());
       int u = Integer.parseInt(st.nextToken());
       int v = Integer.parseInt(st.nextToken());
@@ -33,13 +32,13 @@ public class B11724 {
       graph[v][u] = 1;
     }
     
-    for(int i=1; i<=N; i++){
-      if(!visited[i]){
+    for (int i = 1; i <= N; ++i) {
+      if (!visited[i]) {
         dfs(i);
-        conectedComponent++;
+        ++connectedComponent;
       }
     }
-    System.out.println(conectedComponent);
+    System.out.println(connectedComponent);
   }
 }
 
